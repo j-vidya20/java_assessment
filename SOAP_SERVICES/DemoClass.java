b4a866c28434c3ba55a9f0ab594d94b0d78858b5
J sai sree vidya <52270127+j-vidya20@users.noreply.github.com>
2021-05-06 20:39:43 +0530

    Add files via upload

---
+++ SOAP_SERVICES/DemoClass.java
@@ -0,0 +1,22 @@
+package com.soapadd.services;
+
+
+	public class DemoClass {
+		public String add(int a,int b){
+			return String.valueOf(a+b);
+		}
+		public String sub(int a,int b) {
+			return String.valueOf(a-b);
+			
+			
+		}
+		public String mul(int a,int b) {
+			return String.valueOf(a*b);
+	}
+		public String div(int a,int b) {
+			return String.valueOf(a/b);
+	}
+		
+	}
+
+
